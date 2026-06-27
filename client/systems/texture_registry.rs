5804ad1c45b035bbc8f3e520ae2eeb0a0df42723
Theora59-dev <clem6kiruagon@gmx.fr>
2026-06-27 22:45:40 +0200

    Ajout de textures + modifications TextureRegistry pour pouvoir être plus flexible

--- client/systems/texture_registry.rs
+++ client/systems/texture_registry.rs
@@ -9,10 +9,8 @@ pub struct TextureRegistry;
 
 impl TextureRegistry {
     pub fn register(texture_manager: &mut TextureManager, path: String, render_mode: RenderMode) -> Result<TextureID, Error> {
-        let Ok(texture) = image::open(path) else {
-            return Err(Error::msg("idk"));
-        };
-
-        texture_manager.register_array(render_mode, texture.as_bytes())
+        let texture = image::open(path).map_err(|_| Error::msg("idk"))?;
+        let rgba = texture.into_rgba8();
+        texture_manager.register_array(render_mode, &rgba.into_raw())
     }
 }
